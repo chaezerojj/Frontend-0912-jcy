58e8cea5146cbac6f984d5299b0736f974b16b2b
chaezerojj <yyzz1219@gmail.com>
2023-11-07 14:01:14 +0900

    .

---
+++ JavaScript/1102/z_review/script.js
@@ -0,0 +1,17 @@
+// prompt 창으로 입력받은 값은
+// 기본적으로 문자형타입으로 지정
+// 문제 1
+
+let r;
+r = 10;
+PI = 3.14;
+
+console.log(PI * r * r);
+
+// 문제2
+
+let inch;
+let cm;
+inch =
+inch = prompt('숫자를 입력하세요.');
+
