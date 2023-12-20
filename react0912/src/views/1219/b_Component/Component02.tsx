ec781f4dcef619bc5e2796e23bbd9ba4af1bb021
chaezerojj <yyzz1219@gmail.com>
2023-12-20 19:36:58 +0900

    20231219

---
+++ react0912/src/views/1219/b_Component/Component02.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Component01 from "./Component01";
+
+export default function Component02 () {
+  return (
+    <div>
+      <Component01 />
+      <Component02 />
+    </div>
+  )
+}
