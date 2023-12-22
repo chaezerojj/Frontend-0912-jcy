742b609768647492851d3bd3498a648b6ea88266
chaezerojj <yyzz1219@gmail.com>
2023-12-22 17:50:13 +0900

    20231222

---
+++ react0912/src/views/1222/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import UseState01 from './a_useState/UseState01'
+import UseState02 from './a_useState/UseState02'
+import UseState03 from './a_useState/UseState03'
+import UseState04 from './a_useState/UseState04'
+
+export default function Index() {
+  return (
+    <>
+      <h1>1222 수업 자료</h1>
+      <h2>1. useState</h2>
+      <UseState01 />
+      <h2>2. useState 이벤트 처리 </h2>
+      <UseState02 />
+      <h2>3. </h2>
+      <UseState03 />
+      <h2>4. </h2>
+      <UseState04 />
+    </>
+  )
+}
